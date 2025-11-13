61a01631526006abe682346978ba56476a5e4e6c
Hans Naert <hans.naert@vives.be>
2025-11-14 00:28:32 +0100

    Add TodoItem model, context and generate controller with EF

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
